4e830367d1184ddaba0d8990a8483c8ff05f8b12
Doluker <kakafka1842@gmail.com>
2026-02-12 21:35:05 +0700

    added DTOs and configure Gradle

---
+++ app/src/main/java/com/example/collegeschedulemobile/data/dto/LessonGroupPart.kt
@@ -0,0 +1,2 @@
+package com.example.collegeschedulemobile.data.dto
+enum class LessonGroupPart { FULL, SUB1, SUB2 }
---
+++ app/src/main/java/com/example/collegeschedulemobile/data/dto/ScheduleByDateDto.kt
@@ -0,0 +1,7 @@
+package com.example.collegeschedulemobile.data.dto
+
+data class ScheduleByDateDto(
+    val lessonDate: String,
+    val weekday: String,
+    val lessons: List<LessonDto>
+)
